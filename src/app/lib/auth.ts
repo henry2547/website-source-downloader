3fb108e9c2de21001fc96e9f4d14fe507367cb0e
Henry Muchiri <76833536+henry2547@users.noreply.github.com>
2025-06-27 04:55:06 +0300

    Delete src/app/lib directory

--- src/app/lib/auth.ts
+++
@@ -1,7 +0,0 @@
-import { getServerSession } from 'next-auth';
-import { authOptions } from '../api/auth/[...nextauth]/route';
-
-export async function getCurrentUser() {
-  const session = await getServerSession(authOptions);
-  return session?.user;
-}
